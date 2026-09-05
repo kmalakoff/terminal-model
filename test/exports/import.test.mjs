9fd93c22ae7355208ad29c6ceb0921ca2af72f6e
Kevin Malakoff <kmalakoff@gmail.com>
2026-09-04 18:59:46 -0700

    Upgrade modules

---
+++ test/exports/import.test.mjs
@@ -0,0 +1,23 @@
+import assert from 'assert';
+import { ImmediateStrategy, SgrComposer, StatefulTimeoutStrategy, StreamingTerminal, TerminalTransform, TimeoutStrategy } from 'terminal-model';
+
+describe('exports .mjs', () => {
+  it('SgrComposer', () => {
+    assert.equal(typeof SgrComposer, 'function');
+  });
+  it('StreamingTerminal', () => {
+    assert.equal(typeof StreamingTerminal, 'function');
+  });
+  it('ImmediateStrategy', () => {
+    assert.equal(typeof ImmediateStrategy, 'function');
+  });
+  it('StatefulTimeoutStrategy', () => {
+    assert.equal(typeof StatefulTimeoutStrategy, 'function');
+  });
+  it('TimeoutStrategy', () => {
+    assert.equal(typeof TimeoutStrategy, 'function');
+  });
+  it('TerminalTransform', () => {
+    assert.equal(typeof TerminalTransform, 'function');
+  });
+});
